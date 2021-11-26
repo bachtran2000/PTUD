fe65617219f2621bf4af734c804d0258c4960056
Bách Trần Cao Minh <bachtran.2000@gmail.com>
2021-11-27 04:31:19 +0700

    first commit

---
+++ LoginBasic/src/main/java/LoginController.java
@@ -0,0 +1,61 @@
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author bachtrancaominh
+ */
+public class LoginController {
+
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/user?serverTimezone=UTC";
+    private static final String DATABASE_USER_NAME = "root";
+    private static final String DATABASE_PASSWORD = "";
+
+    public LoginController() {
+    }
+
+    public static Connection DBConnect() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(DATABASE_URL, DATABASE_USER_NAME, DATABASE_PASSWORD);
+
+            return con;
+        } catch (Exception e) {
+            System.out.println("Fail :<");
+        }
+        return null;
+    }
+
+    public boolean checkLogin(String username, String password) throws ClassNotFoundException, SQLException {
+
+        Connection conn = DBConnect();
+        java.sql.Statement stm = conn.createStatement();
+        String sql = "Select * from nhanvien where username='" + username + "' and password='" + password + "'";
+        ResultSet rs = stm.executeQuery(sql);
+
+        if (rs.next()) {
+            System.out.println("Successfull !!!");
+            return true;
+        } else {
+            System.out.println("Fail :<");
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        LoginForm lg = new LoginForm();
+        lg.show();
+
+    }
+
+}
